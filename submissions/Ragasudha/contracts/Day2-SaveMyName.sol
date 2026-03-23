d00b21952832abeae5d0b90b71e0ab9b74790874
RAGASUDHA B <ragasudha@student.tce.edu>
2026-03-23 16:23:38 +0000

    add day 2 savemyname solution

---
+++ submissions/Ragasudha/contracts/Day2-SaveMyName.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+contract SaveMyName{
+    string public name;
+    string public bio;
+    bool public isActive;
+    function setProfile(string memory _name,string memory _bio) public{
+        name=_name;
+        bio=_bio;
+        isActive=true;
+    }
+    function getProfile() public view returns (string memory,string memory,bool){
+        return (name,bio,isActive);
+    }
+}
